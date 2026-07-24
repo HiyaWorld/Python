3bbf3131086db8431b2d88afa9d517dacc8a700a
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-07-24 15:33:16 +0100

    Add Turtle graphics code to draw a pentagon

---
+++ Pentagon.py
@@ -0,0 +1,12 @@
+import turtle
+
+length_line = 200
+angle_of_turn = 72
+turtle.pencolor("gold")
+
+for i in range(5):
+    turtle.forward(length_line)
+    turtle.left(angle_of_turn)
+    
+
+turtle.exitonclick()

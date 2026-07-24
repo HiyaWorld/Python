d5ddb3bb4c8d543f720c0037009a499f3d1de87f
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-07-24 15:35:25 +0100

    Add FilledCircle.py to draw a filled circle

---
+++ FilledCircle.py
@@ -0,0 +1,12 @@
+import turtle
+
+length_line = 150
+angle_of_turn = 60
+turtle.pencolor("gold")
+turtle.fillcolor("silver")
+turtle.begin_fill()
+turtle.bgcolor("grey")
+turtle.circle(100)
+turtle.end_fill()
+
+turtle.exitonclick()

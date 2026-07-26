fe2eecaf04a916290ef2a5bed792c013c8e55160
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-07-26 17:21:48 +0100

    Add Flower18Petal.py to draw a flower using turtle

---
+++ Flower18Petal.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.pencolor("gold")
+
+for i in range (18):
+    turtle.circle(120 ,70)
+    turtle.left(110)
+    turtle.circle(120 ,70)
+    turtle.left(130)
+
+turtle.exitonclick()
